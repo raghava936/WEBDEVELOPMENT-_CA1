add9603f92e21d305a11491f3c834c8f6c5ba3f9
raghava936 <166298613+raghava936@users.noreply.github.com>
2024-07-24 17:31:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,14 @@ dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
-
+app.use(
+	cors({
+		origin: [
+			"https://webdevelopment-ca1-us4k.onrender.com",
+		],
+		credentials: true,
+	})
+);
 
 mongoose.connect("mongodb+srv://anujkurmi199:8pnKtuIs5QW80kJO@cluster0.38bwibw.mongodb.net/Bloodbank", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (e) => {
 	console.log(e ? e : "Connected successfully to database");
